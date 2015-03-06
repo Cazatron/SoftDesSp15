7e3371749297ea8d693edf8417aa1cf63a105755
Caz Nichols <caz.nichols@students.olin.edu>
2015-03-06 12:00:37 -0500

    machine learning exercise and questions completed

--- toolbox/ml/learning_curve.py
+++ toolbox/ml/learning_curve.py
@@ -7,17 +7,23 @@ from sklearn.cross_validation import train_test_split
 from sklearn.linear_model import LogisticRegression
 
 data = load_digits()
-print data.DESCR
+#print data.DESCR
 num_trials = 10
 train_percentages = range(5,95,5)
 test_accuracies = numpy.zeros(len(train_percentages))
 
-# train a model with training percentages between 5 and 90 (see train_percentages) and evaluate
-# the resultant accuracy.
-# You should repeat each training percentage num_trials times to smooth out variability
-# for consistency with the previous example use model = LogisticRegression(C=10**-10) for your learner
+scores = []
+test_accuracies = []
+num_trials = 500
 
-# TODO: your code here
+for i in train_percentages:
+	X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, train_size = i/100.0)
+	model = LogisticRegression(C = 10**-10)
+	model.fit(X_train, y_train)
+	for j in range(num_trials):
+		scores.append(model.score(X_test, y_test))
+
+	test_accuracies.append(sum(scores)/10)
 
 fig = plt.figure()
 plt.plot(train_percentages, test_accuracies)
