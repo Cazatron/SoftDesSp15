259f3d868560024ff57df1ca4161768bb590a649
Caz Nichols <caz.nichols@students.olin.edu>
2015-03-07 18:36:42 -0500

    played with note duration and backing track

--- toolbox/algorithmic_composition/blues_solo.py
+++ toolbox/algorithmic_composition/blues_solo.py
@@ -9,7 +9,7 @@ def add_note(out, instr, key_num, duration, bpm, volume):
 
         out: the stream to add the note to
         instr: the instrument that should play the note
-        key_num: the piano key number (A 440Hzz is 49)
+        key_num: the piano key number (A 440Hz is 49)
         duration: the duration of the note in beats
         bpm: the tempo of the music
         volume: the volume of the note
@@ -31,6 +31,30 @@ solo = AudioStream(sampling_rate, 1)
 blues_scale = [25, 28, 30, 31, 32, 35, 37, 40, 42, 43, 44, 47, 49, 52, 54, 55, 56, 59, 61]
 beats_per_minute = 45				# Let's make a slow blues solo
 
-add_note(solo, bass, blues_scale[0], 1.0, beats_per_minute, 1.0)
+curr_note = 0
+add_note(solo, bass, blues_scale[curr_note], 1.0, beats_per_minute, 1.0)
+licks = [[[1, 0.25], [-1, 0.25], [1, 0.25], [1, 1.25]], [[-1, 0.5], [-1, 0.5], [-1, 0.5], [-1, 0.5]], [[4, 0.5], [-4, 0.5], [3, 0.5], [1, 0.5]], [[1, 0.25], [-1, 0.25], [-1, 0.25], [-2, 0.25], [2, 0.25], [1, 0.25], [1, 0.5]], [[-3, 0.25], [-1, 0.25], [-2, 0.25], [1, 0.25], [-4, 0.5], [1, 0.5]]]
 
-solo >> "blues_solo.wav"
+for i in range(10):
+    lick = choice(licks)
+    for note in lick:
+        curr_note += note[0]
+        if curr_note < 0 or curr_note > len(blues_scale) - 1:
+            curr_note -= 2 * note[0]
+        add_note(solo, bass, blues_scale[curr_note], note[1], beats_per_minute, 1.0)
+
+add_note(solo, bass, blues_scale[6], note[1], beats_per_minute, 4.0)
+
+#solo >> "blues_solo.wav"
+backing_track = AudioStream(sampling_rate, 1)
+Wavefile.read('backing.wav', backing_track)
+
+m = Mixer()
+
+solo *= 0.6            #adjust volumes
+backing_track *= 1.7
+
+m.add(2.25, 0, solo)
+m.add(0,0, backing_track)
+
+m.getStream(500.0) >> "slow_blues.wav"
